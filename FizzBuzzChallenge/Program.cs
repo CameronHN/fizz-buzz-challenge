45a90dda1a05111f9ba0b8bc5ab906f16d975365
Cameron Noemdo <cameronnoemdo@outlook.com>
2024-11-05 10:47:24 +0200

    Initial commit

---
+++ FizzBuzzChallenge/Program.cs
@@ -0,0 +1,13 @@
+﻿string fizz = "Fizz";
+string buzz = "Buzz";
+
+for (int i = 1; i <= 100; i++) {
+    if ((i % 3 == 0) && (i % 5 == 0))
+        System.Console.WriteLine($"{i} - {fizz}{buzz}");
+    else if (i % 3 == 0)
+        System.Console.WriteLine($"{i} - {fizz}");
+    else if (i % 5 == 0)
+        System.Console.WriteLine($"{i} - {buzz}");
+    else
+        System.Console.WriteLine("" + i);
+}
